d55129d3930f70e8674e71141da9e5bd6fd5ae5e
Christopher Roy <christopher.roy@mail.mcgill.ca>
2019-08-19 22:58:04 -0400

    added joining tables + recipe table schemas

--- main.py
+++ main.py
@@ -1,5 +1,53 @@
 import sqlite3
 
+def create_food_item_table():
+    item_schema = "CREATE TABLE tbl_item(
+                        item_id integer PRIMARY KEY,
+                        name text,
+                        unit text,
+                        base_quantity real,
+                        description text)"
+
+def create_vendor_table():
+    vendor_schema = "CREATE TABLE tbl_vendor(
+                    vendor_id integer PRIMARY KEY,
+                    name text,
+                    creation_date TEXT)"
+
+def create_food_order_table():
+    food_order_schema = "CREATE TABLE tbl_order(
+                        order_id INTEGER PRIMARY KEY,
+                        order_date TEXT,
+                        vendor_id INTEGER NOT NULL,
+                        FOREIGN KEY (vendor_id) REFERENCES tbl_vendor(vendor_id)"
+
+def create_food_order_item_table():
+    food_order_item_schema = "CREATE TABLE tbl_order_item(
+                              id INTEGER PRIMARY KEY,
+                              order_id INTEGER,
+                              item_id INTEGER,
+                              FOREIGN KEY (order_id) REFERENCES tbl_order(order_id),
+                              FOREIGN KEY (item_id)  REFERENCES tbl_item(item_id)
+    )"
+
+def create_recipe_table():
+    recipe_schema = "CREATE TABLE tbl_recipe(
+                     recipe_id INTEGER PRIMARY KEY,
+                     name TEXT,
+                     creation_date TEXT,
+                     last_modified TEXT,
+    )"
+
+def create_recipe_item_table():
+    recipe_item_schema = "CREATE TABLE tbl_recipe_item(
+                         recipe_item_id INTEGER PRIMARY KEY,
+                         recipe_id INTEGER,
+                         item_id INTEGER,
+                         FOREIGN KEY (item_id)  REFERENCES tbl_item(item_id),
+                          FOREIGN KEY (recipe_id)  REFERENCES tbl_recipe(recipe_id)
+    )"
+
+
 def sql_connection():
     try:
         con = sqlite3.connect('foodcost.db')
@@ -11,11 +59,11 @@ def sql_connection():
 
 def sql_table(con):
     cursorObj = con.cursor()
-    cursorObj.execute("CREATE TABLE tbl_fooditem(id integer PRIMARY KEY, name text, description text)")
+    cursorObj.execute()
 
     con.commit()
 
 def init_database():
-    item_schema = "CREATE TABLE "
-    vendor_schema = ""
-    order_schema = ""
+    create_food_item_schema()
+    create_vendor_table()
+    create_food_order_table()
